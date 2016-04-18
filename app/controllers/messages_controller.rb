b781dcd779447b484e4fe88229aca26e6f89988d
takatsu61 <kosuke.takatsu@gmail.com>
2016-04-18 16:23:42 +0000

    4

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -41,7 +41,7 @@ class MessagesController < ApplicationController
 
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   ## ここまで
   
